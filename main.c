b36dd4c580d4f1c155f14f22cc8a0419e03beb7e
DoGooder9000 <mike@rogness.net>
2025-03-02 10:04:27 -0600

    Added Player.h and Settings.h

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <math.h>
 
-const char* Window_Title = "3D Renderer in C";
-int Window_Width = 600;
-int Window_Height = 600;
+#include "src/headers/settings.h"
+#include "src/headers/player.h"
+
+PlayerCharacter Player;
 
 bool running = true;
 
 int main(int argc, char* argv[]){
 	SDL_Init(SDL_INIT_EVERYTHING);
 
-	SDL_Window* window = SDL_CreateWindow(Window_Title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, Window_Width, Window_Height, SDL_WINDOW_SHOWN);
+	SDL_Window* Window;
+	SDL_Renderer* Renderer;
+
+	Window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+	Renderer = SDL_CreateRenderer(Window, -1, 0);
 
 	SDL_Event e;
 	while (running){
@@ -21,7 +27,7 @@ int main(int argc, char* argv[]){
 		}
 	}
 
-	SDL_DestroyWindow(window);
+	SDL_DestroyWindow(Window);
 
 	SDL_Quit();
 	
---
+++ src/headers/player.h
@@ -0,0 +1,8 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+typedef struct{
+	int angle;	// Angles are always going to be in radians, because of the math funciton in math.h
+} PlayerCharacter;
+
+#endif PLAYER_H
